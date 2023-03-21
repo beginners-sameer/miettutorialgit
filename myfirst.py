de89e0abbcc5fa781aeba3a7294f170470e47c4d
i-am-smf <fahadguy8@gmail.com>
2023-03-21 15:13:05 +0530

    Ordered the lines

--- myfirst.py
+++ myfirst.py
@@ -1,16 +1,6 @@
-print("helloword")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("good afternoon")
-=======
+print("Hello World")
 print("Hello SMF")
-print("im gokul")
->>>>>>> 8638aeda2a795cbe400f68a855a4f82fd412293c
-print("good afternoon")
-=======
+print("I am Gokul")
 print("pavi")
-print("Hello SMF")
-print("im gokul")
 print("Hello Mohamed Anas")
 print("im deepak")
->>>>>>> 379faa36969fede99de94f22ff8d64b4be7eb1f2
