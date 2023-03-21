ba1f6a956e64742f2b1d5a3d309ec61bfc25bd86
• BHARATHI • <team.ak.freefire@gmail.com>
2023-03-21 15:15:24 +0530

    New Statement

--- myfirst.py
+++ myfirst.py
@@ -1,16 +1,2 @@
-print("helloword")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("good afternoon")
-=======
-print("Hello SMF")
-print("im gokul")
->>>>>>> 8638aeda2a795cbe400f68a855a4f82fd412293c
-print("good afternoon")
-=======
-print("pavi")
-print("Hello SMF")
-print("im gokul")
-print("Hello Mohamed Anas")
-print("im deepak")
->>>>>>> 379faa36969fede99de94f22ff8d64b4be7eb1f2
+print("There's Nothing Here!!!")
+
