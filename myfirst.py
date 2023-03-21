07184cfea6545ad82a5a5cf2a682d388d82d41b1
ejas <mohamedejas612@gmail.com>
2023-03-21 15:37:52 +0530

    s

--- myfirst.py
+++ myfirst.py
@@ -8,5 +8,6 @@ print("im gokul")
 >>>>>>> 8638aeda2a795cbe400f68a855a4f82fd412293c
 =======
 print("onii chan")
-print("ejas,kalam,thasleem,arunkumar")
+print("ejas,kalam,thasleem,arunkumar",aljath)
 >>>>>>> cb5f6da27372668f1ea8a571cae3d42a0b1493e0
+
