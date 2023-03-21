867252d78c4c8cd9823b1f7795de9eb4b12c6469
jmohamedanas <jmohameanas55@gmail.com>
2023-03-21 16:04:25 +0530

    modified lines

--- myfirst.py
+++ myfirst.py
@@ -3,11 +3,11 @@
 print("helloword hi")
 =======
 print("helloword")
+print("hello mohamed anas")
 print("Hello SMF")
 print("im gokul")
 >>>>>>> 8638aeda2a795cbe400f68a855a4f82fd412293c
 =======
 print("onii chan")
-print("ejas,kalam,thasleem,arunkumar",aljath)
+print("ejas,kalam,thasleem,arunkumar")
 >>>>>>> cb5f6da27372668f1ea8a571cae3d42a0b1493e0
-
