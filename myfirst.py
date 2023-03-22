5de1f2c810e9df573b87d04b8dd1a4722166ea9d
hari <hari@gmail.com>
2023-03-22 21:23:08 +0530

    new change

--- myfirst.py
+++ myfirst.py
@@ -6,6 +6,7 @@ print("helloword")
 print("hello mohamed anas")
 print("Hello SMF")
 print("im gokul")
+print("elon musk")
 >>>>>>> 8638aeda2a795cbe400f68a855a4f82fd412293c
 =======
 print("onii chan")
